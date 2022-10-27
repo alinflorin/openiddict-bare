b7b43bffbc9271cdc36e66a607eb754ef4ee4bd6
Alin Florin <alin@qoffice.ro>
2022-10-27 15:35:49 +0300

    -

--- OpenIddictBare/Models/Templates.cs
+++ OpenIddictBare/Models/Templates.cs
@@ -10,7 +10,7 @@ namespace OpenIddictBare.Models
             var linksHtml = "";
             foreach (var provider in providers) {
                 var linkWithProvider = QueryHelpers.AddQueryString(link, "provider", provider);
-                linksHtml += $"<li><a href=\"{linkWithProvider}\">{provider}</a></li>";
+                linksHtml += $"<li class=\"provider-{provider}\"><a href=\"{linkWithProvider}\">{provider}</a></li>";
             }
             return LoginTemplate.Replace("{{links}}", linksHtml);
         }
